fc7d26a8d71bfb50372b6924102f7093c0406532
Daniel Melo <kaynarasilva@MacBook-Air-de-Kaynara.local>
2021-06-30 08:05:52 -0300

    queries costomizadas e arquivo META-INF/orm.xml para armazenar consultas

--- src/main/java/com/algaworks/algafood/api/cotroller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/cotroller/TesteController.java
@@ -2,8 +2,10 @@ package com.algaworks.algafood.api.cotroller;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,5 +34,25 @@ public class TesteController {
 	@GetMapping("/restaurantes/por-taxa-frete")
 	public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxainicial, BigDecimal taxafinal) {
 		return restauranteRepository.findByTaxaFreteBetween(taxainicial, taxafinal);
+	}
+	
+	@GetMapping("/restaurantes/por-nome")
+	public List<Restaurante> restaurantePorNome(String nome, Long cozinha) {
+		return restauranteRepository.consultarPorNome(nome, cozinha);
+	}
+	
+	@GetMapping("/restaurantes/dois-por-nome")
+	public List<Restaurante> restaurantesPorNome(String nome) {
+		return restauranteRepository.findTop2ByNomeContaining(nome);
+	}
+	
+	@GetMapping("/restaurantes/existe-por-nome")
+	public boolean restauranteExistePorNome(String nome) {
+		return restauranteRepository.existsByNome(nome);
+	}
+	
+	@GetMapping("/restaurantes/contador-por-cozinha")
+	public int restaurantePorCozinha(Long cozinha) {
+		return restauranteRepository.countByCozinhaId(cozinha);
 	}
 }
